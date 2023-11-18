aa7c5927b848eef497eab3af4426bd757e12c16f
jais-ravi <ravijaiswal258852@gmail.com>
2023-11-18 13:57:51 +0530

    file uploaded

---
+++ index.js
@@ -0,0 +1,18 @@
+// ******** Auto typing **********
+
+var typed = new Typed(".auto-type",{
+    strings : ["Web Developer","Video Editior","Photographer","Graphic Designer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop : true,
+    
+})
+
+
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+mobile_nav.addEventListener("click", () => {
+    headerElem.classList.toggle("active");
+});
