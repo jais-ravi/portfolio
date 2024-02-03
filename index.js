38cc89d614ab3c94657a26e88e54fdc63f547869
Ravi jaiswal <132082680+jais-ravi@users.noreply.github.com>
2024-02-03 23:44:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var typed = new Typed(".auto-type",{
     
 })
 
-
+// ************ Navbar hamberger button.. *****************
 
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const headerElem = document.querySelector(".header");
